a92a59680e694c7b7bfce627fe53a56592d30d2c
Pablo Thasi <thasipablo@gmail.com>
2023-10-06 11:45:18 +0100

    fix lint errors

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -2,13 +2,13 @@ module MyEnumerable
   def all?(&block)
     each { |el| return false unless block.call(el) }
     true
-  end 
-  
+  end
+    
   def any?(&block)
     each { |el| return true if block.call(el) }
     false
   end
-  
+    
   def filter(&block)
     result = []
     each { |el| result << el if block.call(el) }
